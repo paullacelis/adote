0d5ea7fc749e488f4e65e6b2d2850af895510996
Ana Paula Celis <paullacelis@yahoo.com.br>
2023-06-30 18:21:31 -0300

    fase 2
    atualizacao divulgar

--- adote/settings.py
+++ adote/settings.py
@@ -28,8 +28,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios'
-]
+    'usuarios',
+    'divulgar'     
+
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
