5d762c4d24514368afae5b026f4f4d8dd8604441
Ana Paula Celis <paullacelis@yahoo.com.br>
2023-07-06 20:58:53 -0300

    atualizacao

--- divulgar/views.py
+++ divulgar/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import path
+from django.http import HttpResponse 
+from . import views
+from django.contrib.auth.decorators import login_required
+from . models import Tag, Raca, Pet 
 
+
+
+@login_required
 def novo_pet(request):
-    return HttpResponse('novo pet')
+   if request.method == "GET":
+      tags = Tag.objects.all()
+      racas = Raca.objects.all()
+      return render(request, 'novo_pet.html', {'tags':tags, 'racas': racas})
+
+   elif request.method == "POST":
+         foto = request.FILES.get('foto')
+         nome = request.POST.get('nome')
+         descricao = request.POST.get('descricao')
+         estado = request.POST.get('estado')
+         cidade = request.POST.get('cidade')
+         telefone = request.POST.get('telefone')
+         tags = request.POST.getlist('tags')
+         raca = request.POST.get('raca')
+
+ #TODO: Validar dados
+
+         pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+      )
+
+         pet.save()
+        
+         for tag_id in tags:
+             tag = Tag.objects.get(id=tag_id)
+             pet.tags.add(tag)
+
+         pet.save()
+
+         tags = Tag.objects.all()
+         racas = Raca.objects.all()
+         messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
+         return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+        
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from . models import Raca,Tag,Pet
-
-
-admin.site.register(Raca)
-admin.site.register(Tag)
-admin.site.register(Pet)
